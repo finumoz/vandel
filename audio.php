bb548f1587063fb93c0a431c56184398a5a10731
finumoz <42862001+finumoz@users.noreply.github.com>
2019-04-06 00:56:02 +0300

    Add files via upload

---
+++ audio.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>audio</title>
+  <link rel="stylesheet" href="css/style.css">
+  
+ </head>
+ <body>
+ 
+ <div class="tracks">
+<p>С УТРА</p>
+  <audio controls="controls">
+    <source src="audio/sutra.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером. 
+    <a href="audio/sutra.mp3">Скачайте музыку</a>
+  </audio>
+  <p>ОБРЕЗ</p>
+  <audio controls="controls">
+    <source src="audio/obrez.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером. 
+    <a href="audio/obrez.mp3">Скачайте музыку</a></audio> 
+	
+<p>БИОГРАФИЯ</p>
+  <audio controls="controls">
+    <source src="audio/bio.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером. 
+    <a href="audio/bio.mp3">Скачайте музыку</a></audio> 
+	 
+	<p>ПОНИМАЕТ КАЖДЫЙ</p>
+  <audio controls="controls">
+    <source src="audio/kazh.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером. 
+    <a href="audio/kazh.mp3">Скачайте музыку</a></audio> 
+	<p>МОНСТР</p>
+  <audio controls="controls">
+    <source src="audio/monstr.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером. 
+    <a href="monstr/kazh.mp3">Скачайте музыку</a></audio>  
+		<p>С БЕНЗОПИЛОЙ</p>
+  <audio controls="controls">
+    <source src="audio/sbz.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером. 
+    <a href="monstr/sbz.mp3">Скачайте музыку</a></audio>
+	<p>Солнце за нас</p>
+  <audio controls="controls">
+    <source src="audio/skif.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером. 
+    <a href="monstr/skif.mp3">Скачайте музыку</a></audio>
+	
+</div>		
+ </body>
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+ <head>
+ <link rel="shortcut icon" href="/img/sparta.ico" type="image/x-icon">
+  <meta charset="utf-8">
+  <title>vandal</title>
+   <link rel="stylesheet" href="css/style.css">
+ </head>
+ <body>
+  
+ 
+  <center>
+   
+  <ul id="navbar">
+  <li><a href="audio.php">СЛУШАТЬ</a></li>
+  <li><a href="video.php">СМОТРЕТЬ</a></li>
+  <li><a href="err.php">УМЕРЕТЬ</a></li>
+  
+</ul>
+  </center>
+  <p class="fig"><img src="img/vandal.jpg" 
+  width="580" height="540" alt="Фотография"></p>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+	
+
+	
+	
+	
+ </body>
+</html>
